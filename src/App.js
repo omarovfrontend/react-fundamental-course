2a18ed528ae251dc78285d496ff8af0a485e6f35
Shamil Omarov <s_omarov05@mail.ru>
2022-06-06 02:21:02 +0300

    Сделал счетчик на классах (устаревший метод), полезно знать

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      {/* <Counter/> */}
+      <ClassCounter/>
     </div>
   );
 }
