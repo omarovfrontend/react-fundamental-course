105dbb74134685dbcf4daa18f40e0071eefcc75e
Shamil Omarov <s_omarov05@mail.ru>
2022-06-06 01:54:30 +0300

    Связывание состояния со значением, которая находится в инпуте

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Текст в инпуте');
 
   function increment() {
     setLikes(likes + 1);
@@ -14,6 +15,12 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input 
+        type="text" 
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
