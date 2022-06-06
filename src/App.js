8ade8e28164bb97721732e0f8da00f50aa29e617
Shamil Omarov <s_omarov05@mail.ru>
2022-06-06 18:31:49 +0300

    Сделал добавление постов

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import React, { useRef, useState } from "react";
+import "./styles/App.css";
+// import ClassCounter from "./components/ClassCounter";
+// import Counter from "./components/Counter";
+// import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+// import MyButton from "./components/UI/button/MyButton";
+// import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript1', body: 'Description'},
+    {id: 2, title: 'Javascript2', body: 'Description'},
+    {id: 3, title: 'Javascript3', body: 'Description'},
+  ])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
   return (
     <div className="App">
-      {/* <Counter/> */}
-      <ClassCounter/>
+      <PostForm create={createPost} />
+      <PostList posts={posts} title="Посты про JS!" />
     </div>
   );
 }
