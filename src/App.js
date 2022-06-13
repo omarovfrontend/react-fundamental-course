ae3f5f557a8461ccfdbfd7595aa137df06ab2054
Shamil Omarov <s_omarov05@mail.ru>
2022-06-13 16:12:49 +0300

    Вынес в отдельный хук сортировку и фильтрацию постов

--- src/App.js
+++ src/App.js
@@ -5,27 +5,14 @@ import MyButton from "./components/UI/button/MyButton";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'аа', body: 'бб'},
-    {id: 2, title: 'гг', body: 'аа'},
-    {id: 3, title: 'вв', body: 'яя'},
-  ])
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-  
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
